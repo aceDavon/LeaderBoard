cde1ae7f5793272d9445a48d684fabaaa54c5cdb
aceDavon <makojidavid@gmail.com>
2022-08-24 23:36:41 +0100

    Refactor code to ES6 Arrow functions

--- src/index.js
+++ src/index.js
@@ -3,33 +3,13 @@ import Table from './components/Table.js';
 import { refresh } from './data/createGames.js';
 import './index.css';
 
-// const createGame = async () => {
-//   const url =
-//     'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
-
-//   return await fetch(url, {
-//     method: 'post',
-//     body: JSON.stringify({
-//       name: 'My new cool game',
-//     }),
-//     headers: {
-//       'content-type': 'Application/json; charset=UTF-8'
-//     }
-//   })
-//     .then((Response) => Response.json())
-//     .then((res) => console.log(res))
-//     .catch((err) => console.log(err.message));
-// };
-
-// createGame()
-
-function component() {
+const component = () => {
   const entry = document.getElementById('data_input');
   const refBtn = document.getElementById('refresh');
   refBtn.addEventListener('click', () => refresh());
   Table();
 
   entry.appendChild(Form());
-}
+};
 
 component();
