1718ed5b12d19d37561f22f6a1920a04d3af8954
aceDavon <makojidavid@gmail.com>
2022-08-30 20:15:52 +0100

    Run the build command to compile latest changes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import './index.css';
 const component = () => {
   const entry = document.getElementById('data_input');
   const refBtn = document.getElementById('refresh');
-  const extLink = document.querySelector('.copyright')
+  const extLink = document.querySelector('.copyright');
   refBtn.addEventListener('click', () => refresh());
   Table();
   extLink.addEventListener('click', () => {
-    window.location.href='https://github.com/aceDavon'
-  })
-
+    window.location.href = 'https://github.com/aceDavon';
+  });
 
   entry.appendChild(Form());
 };
